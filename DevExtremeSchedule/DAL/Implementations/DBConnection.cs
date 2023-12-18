76ac99532bf6d47d9f11a4bb034720fadae39179
Danil <dadevildi@gmail.com>
2023-12-18 23:54:18 +0300

    first commit

---
+++ DevExtremeSchedule/DAL/Implementations/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using DevExtremeSchedule.DAL.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace DevExtremeSchedule.DAL.Implementations
+{
+    public class DBConnection: IDBConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public DBConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
